7c3212d0e4cf2aa79e8955ee709c522d74a5f200
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-05-05 12:07:59 +0200

    Add tooltips

--- Editor/GameCameraEditor.cs
+++ Editor/GameCameraEditor.cs
@@ -48,7 +48,13 @@ class GameCameraEditor : Editor {
     public override void OnInspectorGUI() {
         serializedObject.Update();
 
-        EditorGUILayout.PropertyField(mode);
+        EditorGUILayout.PropertyField(
+            mode,
+            new GUIContent(
+                "Mode",
+                "Limited mode creates a dead zone around the target where " +
+                "target can move freely without causing the camera to move. " +
+                "Use 'Camera Limits' to set the dead zone."));
         EditorGUILayout.PropertyField(
             targetTransform,
             new GUIContent("Target",
@@ -64,16 +70,36 @@ class GameCameraEditor : Editor {
             new GUIContent(
                 "Camera Offset",
                 "Default camera position relative to the target."));
-        EditorGUILayout.PropertyField(lookAtPointOffset);
+        EditorGUILayout.PropertyField(
+            lookAtPointOffset,
+            new GUIContent(
+                "Look At Point Offset",
+                "Use to offset target pivot point."));
         EditorGUILayout.PropertyField(followSpeed);
         EditorGUILayout.PropertyField(
             cameraRotationSpeed,
             new GUIContent(
                 "Cam. Rot. Speed",
                 ""));
-        EditorGUILayout.PropertyField(perspectiveChangeSpeed);
-        EditorGUILayout.PropertyField(movementVelocityOffset);
-        EditorGUILayout.PropertyField(cameraLimits);
+        EditorGUILayout.PropertyField(
+            perspectiveChangeSpeed,
+            new GUIContent(
+                "Perspective Change",
+                "When target gets occluded and camera wants to change " +
+                "position to see the target again, this is how fast this " +
+                "will happen."));
+        EditorGUILayout.PropertyField(
+            movementVelocityOffset,
+            new GUIContent(
+                "Movement Velocity Offset",
+                "???\nTarget velocity will be multiplied by this value. " +
+                "In result, this affects camera lerp speed and occlusion " +
+                "offset."));
+        EditorGUILayout.PropertyField(
+            cameraLimits,
+            new GUIContent(
+                "Camera Limits",
+                "Applies in 'Limited' mode."));
         EditorGUILayout.PropertyField(lookAtPointWhenNotVisible);
         EditorGUILayout.PropertyField(
             offsetWhenNotVisible,
