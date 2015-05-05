67bfdd59a08f11ac69e5775e7f8e8ae2c9b8983a
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-05-05 20:15:36 +0200

    Remove some TPPCam properties

--- CameraSetupComponent/TPPCamSetter.cs
+++ CameraSetupComponent/TPPCamSetter.cs
@@ -10,44 +10,11 @@ namespace TPPCamera.CameraSetupComponent {
         [SerializeField]
         private TPPCam cameraCo;
 
-        // limited camera
-        [SerializeField]
-        private Vector2 cameraLimits = new Vector2(5f, 5f);
-
-        [SerializeField]
-        private LayerMask cameraOcclusionLayerMask = 1 << 9;
-
         [SerializeField]
         private Vector3 cameraOffset;
 
         [SerializeField]
-        private float cameraRotationSpeed = 5f;
-
-        [SerializeField]
-        private float followSpeed = 5f;
-
-        [SerializeField]
         private Vector2 lookAtPointOffset;
-
-        // see through camera variables
-        [SerializeField]
-        private Vector3 lookAtPointWhenNotVisible;
-
-        [SerializeField]
-        private Mode mode;
-
-        [SerializeField]
-        private Vector3 movementVelocityOffset;
-
-        [SerializeField]
-        private Vector3 offsetWhenNotVisible;
-
-        [SerializeField]
-        private float perspectiveChangeSpeed = 10f;
-
-        // main variables
-        [SerializeField]
-        private Transform targetTransform;
 
         #endregion FIELDS
     }
--- Editor/TPPCamSetterEditor.cs
+++ Editor/TPPCamSetterEditor.cs
@@ -12,18 +12,8 @@ namespace TPPCamera.CameraSetupComponent {
         #region SERIALIZED PROPERTIES
 
         private SerializedProperty cameraCo;
-        private SerializedProperty cameraLimits;
-        private SerializedProperty cameraOcclusionLayerMask;
         private SerializedProperty cameraOffset;
-        private SerializedProperty cameraRotationSpeed;
-        private SerializedProperty followSpeed;
         private SerializedProperty lookAtPointOffset;
-        private SerializedProperty lookAtPointWhenNotVisible;
-        private SerializedProperty mode;
-        private SerializedProperty movementVelocityOffset;
-        private SerializedProperty offsetWhenNotVisible;
-        private SerializedProperty perspectiveChangeSpeed;
-        private SerializedProperty targetTransform;
 
         #endregion
         #region UNITY MESSAGES
@@ -32,25 +22,9 @@ namespace TPPCamera.CameraSetupComponent {
             cameraCo = serializedObject.FindProperty("cameraCo");
 
             // TPPCam properties.
-            cameraLimits = serializedObject.FindProperty("cameraLimits");
-            cameraOcclusionLayerMask =
-                serializedObject.FindProperty("cameraOcclusionLayerMask");
             cameraOffset = serializedObject.FindProperty("cameraOffset");
-            cameraRotationSpeed =
-                serializedObject.FindProperty("cameraRotationSpeed");
-            followSpeed = serializedObject.FindProperty("followSpeed");
             lookAtPointOffset =
                 serializedObject.FindProperty("lookAtPointOffset");
-            lookAtPointWhenNotVisible =
-                serializedObject.FindProperty("lookAtPointWhenNotVisible");
-            mode = serializedObject.FindProperty("mode");
-            movementVelocityOffset =
-                serializedObject.FindProperty("movementVelocityOffset");
-            offsetWhenNotVisible =
-                serializedObject.FindProperty("offsetWhenNotVisible");
-            perspectiveChangeSpeed =
-                serializedObject.FindProperty("perspectiveChangeSpeed");
-            targetTransform = serializedObject.FindProperty("targetTransform");
         }
 
         public override void OnInspectorGUI() {
@@ -65,70 +39,14 @@ namespace TPPCamera.CameraSetupComponent {
 
             GUILayout.Label("TPPCam Properties", EditorStyles.boldLabel);
 
-            //DrawTargetTransformField();
-            //DrawFollowSpeedField();
-            //DrawCameraRotationSpeedField();
-            //DrawPerspectiveChangeSpeedField();
-            //DrawModeDropdown();
-            //DrawOcclusionLayerMaskDropdown();
             DrawCameraOffsetField();
-            //DrawOffsetWhenNotVisibleField();
             DrawLookAtPointOffsetField();
-            //DrawLookAtPointWhenNotVisibleField();
-            //DrawCameraLimitsField();
-            //DrawMovementVelocityOffsetField();
-
 
             serializedObject.ApplyModifiedProperties();
         }
         #endregion
 
         #region INSPECTOR
-
-        private void DrawMovementVelocityOffsetField() {
-            EditorGUILayout.PropertyField(
-                movementVelocityOffset,
-                new GUIContent(
-                    "Movement Velocity Offset",
-                    "???\nTarget velocity will be scaled by this value. " +
-                    "In result, this affects camera lerp speed and occlusion " +
-                    "offset."));
-        }
-
-        private void DrawCameraLimitsField() {
-            EditorGUILayout.PropertyField(
-                cameraLimits,
-                new GUIContent(
-                    "Camera Limits",
-                    "Applies in 'Limited' mode."));
-        }
-
-        private void DrawLookAtPointWhenNotVisibleField() {
-            EditorGUILayout.PropertyField(
-                lookAtPointWhenNotVisible,
-                new GUIContent(
-                    "Look At Point When Not Visible",
-                    "Use to offset target pivot point when target is occluded."));
-        }
-
-        private void DrawOffsetWhenNotVisibleField() {
-            EditorGUILayout.PropertyField(
-                offsetWhenNotVisible,
-                new GUIContent(
-                    "Offset When Not Visible",
-                    "Offset applied to 'Camera Offset' when target is not " +
-                    "visible to the camera."));
-        }
-
-        private void DrawPerspectiveChangeSpeedField() {
-            EditorGUILayout.PropertyField(
-                perspectiveChangeSpeed,
-                new GUIContent(
-                    "Perspective Speed",
-                    "When target gets occluded and camera wants to change " +
-                    "position to see the target again, this is how fast this " +
-                    "will happen."));
-        }
 
         private void DrawLookAtPointOffsetField() {
             EditorGUILayout.PropertyField(
@@ -144,48 +62,6 @@ namespace TPPCamera.CameraSetupComponent {
                 new GUIContent(
                     "Camera Offset",
                     "Default camera position relative to the target."));
-        }
-
-        private void DrawOcclusionLayerMaskDropdown() {
-            EditorGUILayout.PropertyField(
-                cameraOcclusionLayerMask,
-                new GUIContent(
-                    "Layer Mask",
-                    "If target gets behind an object specified by the layer mask "
-                    +
-                    "it will be considered as occluded."));
-        }
-
-        private void DrawModeDropdown() {
-            EditorGUILayout.PropertyField(
-                mode,
-                new GUIContent(
-                    "Mode",
-                    "Limited mode creates a dead zone around the target where "
-                    +
-                    "target can move freely without causing the camera to move. "
-                    +
-                    "Use 'Camera Limits' to set the dead zone."));
-        }
-
-        private void DrawCameraRotationSpeedField() {
-            EditorGUILayout.PropertyField(
-                cameraRotationSpeed,
-                new GUIContent(
-                    "Cam. Rot. Speed",
-                    ""));
-        }
-
-        private void DrawFollowSpeedField() {
-            EditorGUILayout.PropertyField(followSpeed);
-        }
-
-        private void DrawTargetTransformField() {
-            EditorGUILayout.PropertyField(
-                targetTransform,
-                new GUIContent(
-                    "Target",
-                    "Transform to follow."));
         }
 
         #endregion INSPECTOR
