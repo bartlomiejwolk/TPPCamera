245aa6f8993ccbdc5901197496a76c3b48d36e67
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-05-05 18:21:13 +0200

    Create CameraSetup class

---
+++ CameraSetupComponent/CameraSetup.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using Camera = TPPCamera.CameraComponent.Camera;
+
+namespace TPPCamera.CameraSetupComponent {
+
+    public class CameraSetup : MonoBehaviour {
+
+        [SerializeField]
+        private Camera cameraCo;
+
+    }
+
+}
---
+++ Editor/CameraSetupEditor.cs
@@ -0,0 +1,27 @@
+﻿using TPPCamera.CameraComponent;
+using UnityEditor;
+
+namespace TPPCamera.CameraSetupComponent {
+
+    [CustomEditor(typeof(CameraSetup))]
+    class CameraSetupEditor : Editor {
+
+        private Camera Script { get; set; }
+
+        private SerializedProperty cameraCo;
+
+        private void OnEnable() {
+            cameraCo = serializedObject.FindProperty("cameraCo");
+        }
+
+        public override void OnInspectorGUI() {
+            serializedObject.Update();
+
+            EditorGUILayout.PropertyField(cameraCo);
+
+            serializedObject.ApplyModifiedProperties();
+        }
+
+    }
+
+}
