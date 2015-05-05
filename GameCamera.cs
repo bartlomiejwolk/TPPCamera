4af000f2467bdf82969f00044db524c701d59e58
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-05-05 17:55:05 +0200

    Add classes to TPPCamera.CameraComponent namespace

--- GameCamera.cs
+++ GameCamera.cs
@@ -1,180 +1,190 @@
 using UnityEngine;
 
-// todo create editor class
-public class GameCamera : MonoBehaviour {
+namespace TPPCamera.CameraComponent {
 
-    // modes
+    public class GameCamera : MonoBehaviour {
 
-    #region FIELDS
+        // modes
 
-    // limited camera
-    [SerializeField]
-    private Vector2 cameraLimits = new Vector2(5f, 5f);
+        #region FIELDS
 
-    [SerializeField]
-    private LayerMask cameraOcclusionLayerMask = 1 << 9;
+        // limited camera
+        [SerializeField]
+        private Vector2 cameraLimits = new Vector2(5f, 5f);
 
-    [SerializeField]
-    private Vector3 cameraOffset;
+        [SerializeField]
+        private LayerMask cameraOcclusionLayerMask = 1 << 9;
 
-    [SerializeField]
-    private float cameraRotationSpeed = 5f;
+        [SerializeField]
+        private Vector3 cameraOffset;
 
-    [SerializeField]
-    private float followSpeed = 5f;
+        [SerializeField]
+        private float cameraRotationSpeed = 5f;
 
-    [SerializeField]
-    private Vector2 lookAtPointOffset;
+        [SerializeField]
+        private float followSpeed = 5f;
 
-    // see through camera variables
-    [SerializeField]
-    private Vector3 lookAtPointWhenNotVisible;
+        [SerializeField]
+        private Vector2 lookAtPointOffset;
 
-    [SerializeField]
-    private Mode mode;
+        // see through camera variables
+        [SerializeField]
+        private Vector3 lookAtPointWhenNotVisible;
 
-    [SerializeField]
-    private Vector3 movementVelocityOffset;
+        [SerializeField]
+        private Mode mode;
 
-    [SerializeField]
-    private Vector3 offsetWhenNotVisible;
+        [SerializeField]
+        private Vector3 movementVelocityOffset;
 
-    [SerializeField]
-    private float perspectiveChangeSpeed = 10f;
+        [SerializeField]
+        private Vector3 offsetWhenNotVisible;
 
-    // main variables
-    [SerializeField]
-    private Transform targetTransform;
+        [SerializeField]
+        private float perspectiveChangeSpeed = 10f;
 
-    // camera follow target
-    private Vector3 cameraTarget;
+        // main variables
+        [SerializeField]
+        private Transform targetTransform;
 
-    // velocity check
-    private Vector3 lastTargetPos;
+        // camera follow target
+        private Vector3 cameraTarget;
 
-    // movement
-    private Vector3 smoothCamOffset;
+        // velocity check
+        private Vector3 lastTargetPos;
 
-    private Vector3 targetVelocity;
+        // movement
+        private Vector3 smoothCamOffset;
 
-    #endregion FIELDS
+        private Vector3 targetVelocity;
 
-    #region UNITY MESSAGES
+        #endregion FIELDS
 
-    private void FixedUpdate() {
-        FollowTarget();
-    }
+        #region UNITY MESSAGES
 
-    private void Start() {
-        smoothCamOffset = cameraOffset;
-        cameraTarget = targetTransform.position;
-    }
-
-    #endregion UNITY MESSAGES
-
-    #region METHODS
-
-    public bool DetectOccluders() {
-        // get distance and direction for raycast
-        var cameraOffsetPos = targetTransform.position + cameraOffset;
-        // Ray length decreased by 0.1 to not hit the floor.
-        var tDist = (targetTransform.position - cameraOffsetPos).magnitude - 0.1f;
-        var tDir = (targetTransform.position - cameraOffsetPos).normalized;
-
-        // check if player visible
-        RaycastHit hit;
-        if (Physics.Raycast(
-            cameraOffsetPos + targetVelocity,
-            tDir,
-            out hit,
-            tDist,
-            cameraOcclusionLayerMask)) {
-            return false;
+        private void FixedUpdate() {
+            FollowTarget();
         }
-        return true;
-    }
 
-    protected void FollowTarget() {
-        if (targetTransform == null) return;
-
-        // target speed check
-        targetVelocity = (targetTransform.position - lastTargetPos) / Time.fixedDeltaTime;
-        targetVelocity = Vector3.Scale(targetVelocity, movementVelocityOffset);
-
-        // Camera movement speed.
-        // Increase camera speed along with target speed.
-        var avgVelocity = (Mathf.Abs(targetVelocity.x)
-                           + Mathf.Abs(targetVelocity.y)
-                           + Mathf.Abs(targetVelocity.z)) / 3;
-
-        var lerpSpeed = (avgVelocity + followSpeed) * Time.fixedDeltaTime;
-
-        // save camera rotation
-        var tRot = Quaternion.identity;
-
-        // control camera in Limited mode
-        if (mode == Mode.Limited) {
-            if (Mathf.Abs(targetTransform.position.x - cameraTarget.x) > cameraLimits.x
-                ||
-                Mathf.Abs(targetTransform.position.z - cameraTarget.z) > cameraLimits.y) {
-                cameraTarget += (targetTransform.position - cameraTarget).normalized *
-                                (targetTransform.position - cameraTarget).magnitude
-                                * Time.fixedDeltaTime * followSpeed;
-            }
-            else {
-                targetVelocity = Vector3.zero;
-            }
-            cameraTarget = new Vector3(
-                cameraTarget.x,
-                targetTransform.position.y,
-                cameraTarget.z);
-        }
-        else {
+        private void Start() {
+            smoothCamOffset = cameraOffset;
             cameraTarget = targetTransform.position;
         }
 
-        // detect if player is visible and set offset accordingly
-        var playerVisible = DetectOccluders();
-        var occlusionOffset = cameraOffset;
-        var occlusionLookAtPointOffset =
-            new Vector3(
-                lookAtPointOffset.x,
-                -smoothCamOffset.y,
-                lookAtPointOffset.y);
-        if (!playerVisible) {
-            occlusionOffset = offsetWhenNotVisible;
-            occlusionLookAtPointOffset = new Vector3(
-                lookAtPointWhenNotVisible.x,
-                -smoothCamOffset.y,
-                lookAtPointWhenNotVisible.z);
+        #endregion UNITY MESSAGES
+
+        #region METHODS
+
+        public bool DetectOccluders() {
+            // get distance and direction for raycast
+            var cameraOffsetPos = targetTransform.position + cameraOffset;
+            // Ray length decreased by 0.1 to not hit the floor.
+            var tDist = (targetTransform.position - cameraOffsetPos).magnitude
+                        - 0.1f;
+            var tDir = (targetTransform.position - cameraOffsetPos).normalized;
+
+            // check if player visible
+            RaycastHit hit;
+            if (Physics.Raycast(
+                cameraOffsetPos + targetVelocity,
+                tDir,
+                out hit,
+                tDist,
+                cameraOcclusionLayerMask)) {
+                return false;
+            }
+            return true;
         }
-        occlusionOffset += targetVelocity;
 
-        // control occlusion offsets
-        smoothCamOffset = Vector3.MoveTowards(
-            smoothCamOffset,
-            occlusionOffset,
-            Time.fixedDeltaTime * perspectiveChangeSpeed);
-        tRot.SetLookRotation(
-            (cameraTarget
-            + (occlusionLookAtPointOffset
-            + (transform.position - cameraTarget)))
-            - transform.position,
-            targetTransform.up);
+        protected void FollowTarget() {
+            if (targetTransform == null) return;
 
-        // apply transformations
-        transform.position = Vector3.Lerp(
-            transform.position,
-            cameraTarget + smoothCamOffset,
-            lerpSpeed);
-        transform.rotation = Quaternion.Slerp(
-            transform.rotation,
-            tRot,
-            cameraRotationSpeed * Time.fixedDeltaTime);
+            // target speed check
+            targetVelocity = (targetTransform.position - lastTargetPos)
+                             / Time.fixedDeltaTime;
+            targetVelocity = Vector3.Scale(
+                targetVelocity,
+                movementVelocityOffset);
 
-        // save last target position
-        lastTargetPos = targetTransform.position;
+            // Camera movement speed.
+            // Increase camera speed along with target speed.
+            var avgVelocity = (Mathf.Abs(targetVelocity.x)
+                               + Mathf.Abs(targetVelocity.y)
+                               + Mathf.Abs(targetVelocity.z)) / 3;
+
+            var lerpSpeed = (avgVelocity + followSpeed) * Time.fixedDeltaTime;
+
+            // save camera rotation
+            var tRot = Quaternion.identity;
+
+            // control camera in Limited mode
+            if (mode == Mode.Limited) {
+                if (Mathf.Abs(targetTransform.position.x - cameraTarget.x)
+                    > cameraLimits.x
+                    ||
+                    Mathf.Abs(targetTransform.position.z - cameraTarget.z)
+                    > cameraLimits.y) {
+                    cameraTarget +=
+                        (targetTransform.position - cameraTarget).normalized *
+                        (targetTransform.position - cameraTarget).magnitude
+                        * Time.fixedDeltaTime * followSpeed;
+                }
+                else {
+                    targetVelocity = Vector3.zero;
+                }
+                cameraTarget = new Vector3(
+                    cameraTarget.x,
+                    targetTransform.position.y,
+                    cameraTarget.z);
+            }
+            else {
+                cameraTarget = targetTransform.position;
+            }
+
+            // detect if player is visible and set offset accordingly
+            var playerVisible = DetectOccluders();
+            var occlusionOffset = cameraOffset;
+            var occlusionLookAtPointOffset =
+                new Vector3(
+                    lookAtPointOffset.x,
+                    -smoothCamOffset.y,
+                    lookAtPointOffset.y);
+            if (!playerVisible) {
+                occlusionOffset = offsetWhenNotVisible;
+                occlusionLookAtPointOffset = new Vector3(
+                    lookAtPointWhenNotVisible.x,
+                    -smoothCamOffset.y,
+                    lookAtPointWhenNotVisible.z);
+            }
+            occlusionOffset += targetVelocity;
+
+            // control occlusion offsets
+            smoothCamOffset = Vector3.MoveTowards(
+                smoothCamOffset,
+                occlusionOffset,
+                Time.fixedDeltaTime * perspectiveChangeSpeed);
+            tRot.SetLookRotation(
+                (cameraTarget
+                 + (occlusionLookAtPointOffset
+                    + (transform.position - cameraTarget)))
+                - transform.position,
+                targetTransform.up);
+
+            // apply transformations
+            transform.position = Vector3.Lerp(
+                transform.position,
+                cameraTarget + smoothCamOffset,
+                lerpSpeed);
+            transform.rotation = Quaternion.Slerp(
+                transform.rotation,
+                tRot,
+                cameraRotationSpeed * Time.fixedDeltaTime);
+
+            // save last target position
+            lastTargetPos = targetTransform.position;
+        }
+
+        #endregion METHODS
     }
 
-    #endregion METHODS
 }
