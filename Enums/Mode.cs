b65e9df8416af287ea477988586098238ac7259f
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-05-07 08:25:42 +0200

    Format docs

--- Enums/Mode.cs
+++ Enums/Mode.cs
@@ -1,13 +1,13 @@
 public enum Mode {
 
     /// <summary>
-    /// Camera moves with the target transform instantly.
+    ///     Camera moves with the target transform instantly.
     /// </summary>
     Instantenous,
 
     /// <summary>
-    /// If target transform moves withing defined dead zone, camera
-    /// will not follow.
+    ///     If target transform moves withing defined dead zone, camera
+    ///     will not follow.
     /// </summary>
     DeadZone
 
